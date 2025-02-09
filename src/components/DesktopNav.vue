5e1d8d7ba57de9e330df43612bbd66f9cc1068d6
Matthew Gies <matt.bgies@gmail.com>
2025-02-09 14:27:52 -0800

    added external profile links to DesktopNav. renamed css class to external-profile-link since it's now used on both desktop and mobile

--- src/components/DesktopNav.vue
+++ src/components/DesktopNav.vue
@@ -12,14 +12,39 @@ let activePage: Ref<string> = ref('home');
 <template>
   <nav class="desktop-only">
     <RouterLink
-      v-for="(page, i) of pages"
-      :key="page"
-      @click="activePage = page"
-      :to="page == 'home' ? '/' : '/' + page"
-      :class="['nav-elem', activePage == page ? 'nav-elem-active' : '']"
+      @click="activePage = 'home'"
+      to="/"
+      :class="['nav-elem', activePage == 'home' ? 'nav-elem-active' : '']"
     >
-      {{ page == 'home' ? '' : page.charAt(0).toUpperCase() + page.slice(1) }}
-      <img class="nav-logo" v-if="page == 'home'" src="@/assets/svg/file.svg" />
+      <img class="nav-logo" src="@/assets/svg/file.svg" />
     </RouterLink>
+    <div style="display: flex; flex-direction: row; align-items: center">
+      <RouterLink
+        v-for="(page, i) of pages.filter((page) => page != 'home')"
+        :key="page"
+        @click="activePage = page"
+        :to="'/' + page"
+        :class="['nav-elem', activePage == page ? 'nav-elem-active' : '']"
+      >
+        {{ page == 'home' ? '' : page.charAt(0).toUpperCase() + page.slice(1) }}
+      </RouterLink>
+      <div class="external-profile-links-group">
+        <a
+          class="external-profile-link"
+          href="https://open.spotify.com/user/mattgies?si=046d4cdc03904e13"
+          target="_blank"
+          ><img src="@/assets/svg/Spotify_icon.svg" />
+        </a>
+        <a
+          class="external-profile-link"
+          href="https://www.linkedin.com/in/matthewgies/"
+          target="_blank"
+          ><img src="@/assets/svg/LinkedIn_icon.svg" />
+        </a>
+        <a class="external-profile-link" href="https://github.com/mattgies" target="_blank"
+          ><img src="@/assets/svg/github-mark.svg" />
+        </a>
+      </div>
+    </div>
   </nav>
 </template>
--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -41,23 +41,20 @@ const handleMobileNavOpenClose = () => {
           {{ page.charAt(0).toUpperCase() + page.slice(1) }}
         </RouterLink>
       </div>
-      <div class="mobile-nav-menu-external-profile-links">
+      <div class="external-profile-links-group">
         <a
-          class="mobile-nav-external-profile-link"
+          class="external-profile-link"
           href="https://open.spotify.com/user/mattgies?si=046d4cdc03904e13"
           target="_blank"
           ><img src="@/assets/svg/Spotify_icon.svg" />
         </a>
         <a
-          class="mobile-nav-external-profile-link"
+          class="external-profile-link"
           href="https://www.linkedin.com/in/matthewgies/"
           target="_blank"
           ><img src="@/assets/svg/LinkedIn_icon.svg" />
         </a>
-        <a
-          class="mobile-nav-external-profile-link"
-          href="https://github.com/mattgies"
-          target="_blank"
+        <a class="external-profile-link" href="https://github.com/mattgies" target="_blank"
           ><img src="@/assets/svg/github-mark.svg" />
         </a>
       </div>
